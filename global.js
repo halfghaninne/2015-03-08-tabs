ee17a53f1c2629216a56825c8639567b127ddc79
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-03-09 11:31:10 -0500

    Working mini-project. Great success!

--- global.js
+++ global.js
@@ -1,26 +1,55 @@
-// window.onload function() {
-//
-  showAbout = document.getElementById("about_link");
-  aboutInfo = document.getElementById("about");
-  showAbout.addEventListener("click", dummyFunction)
-  // showAbout.addEventListener("click", show(aboutInfo));
+window.onload = function() {
+
+  var showAbout = document.getElementById("about_link");
+  var aboutBox = document.getElementById("about_link_box");
+  var aboutInfo = document.getElementById("about");
+  showAbout.addEventListener("click", function() {
+    aboutInfo.style.display = "block";
+    concertInfo.style.display = "none";
+    albumInfo.style.display = "none";
+  });
+  showAbout.addEventListener("click", function() {
+    aboutBox.className = "active";
+    concertBox.className = "inactive";
+    albumsBox.className = "inactive";
+  });
+
+  var showConcerts = document.getElementById("concerts_link");
+  var concertBox = document.getElementById("concerts_link_box");
+  var concertInfo = document.getElementById("concerts");
+  showConcerts.addEventListener("click", function() {
+    aboutInfo.style.display = "none";
+    concertInfo.style.display = "block";
+    albumInfo.style.display = "none";
+  });
+  showConcerts.addEventListener("click", function() {
+    aboutBox.className = "inactive";
+    concertBox.className = "active";
+    albumsBox.className = "inactive";
+  });
+
+  var showAlbums = document.getElementById("albums_link");
+  var albumsBox = document.getElementById("albums_link_box")
+  var albumInfo = document.getElementById("discography");
+  showAlbums.addEventListener("click", function() {
+    aboutInfo.style.display = "none";
+    concertInfo.style.display = "none";
+    albumInfo.style.display = "block";
+  });
+  showAlbums.addEventListener("click", function() {
+    aboutBox.className = "inactive";
+    concertBox.className = "inactive";
+    albumsBox.className = "active";
+  });
+
   
-  showConcerts = document.getElementById("concerts_link");
-  concertInfo = document.getElementById("concerts");
-  showConcerts.addEventListener("click", show(concertInfo));
-  
-  showAlbums = document.getElementById("albums_link");
-  albumInfo = document.getElementById("discography");
-  showAlbums.addEventListener("click", show(albumInfo));
-  
-  
-  function dummyFunction(){
-    showAbout.style.fontWeight = "900";
-  }
-  
+  // function dummyFunction(){
+  //   showAbout.style.fontWeight = "900";
+  // }
+  //
   // function show(elements) {
   //   elements.style.display = "block";
   //
   // } //end of showInfo function
-  //
-// } //end of on.load
+
+} //end of on.load
